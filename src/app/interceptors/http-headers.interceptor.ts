1dd8a76acb939a9993b4fda2286173f8af8d467d
rakesh1673 <59076443+rakesh1673@users.noreply.github.com>
2021-08-07 16:11:56 +0530

    Update http-headers.interceptor.ts

--- src/app/interceptors/http-headers.interceptor.ts
+++ src/app/interceptors/http-headers.interceptor.ts
@@ -17,11 +17,11 @@ export class HttpHeadersInterceptor implements HttpInterceptor {
   ): Observable<HttpEvent<any>> {
     request = request.clone({
       setHeaders: {
-        'x-rapidapi-key': '8b96202ee5msh56e7fc282f09b58p109513jsnf3ec6a0731a7',
+        'x-rapidapi-key': '***********************************************',
         'x-rapidapi-host': 'rawg-video-games-database.p.rapidapi.com',
       },
       setParams: {
-        key: '4f4f9744e2e3462181e80eb1c9e25194',
+        key: '***************************************',
       },
     });
     return next.handle(request);
